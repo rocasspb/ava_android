9195417cc96d31e9820524b856d2aa56a995ae10
rocas <rocas.spb@gmail.com>
2026-05-14 17:27:39 +0200

    feat(web): Create KMP artifacts directory and copy Wasm/JS files

---
+++ web/src/kmp/AvaAwaAnd-shared.mjs
@@ -0,0 +1,13 @@
+
+import { instantiate } from './AvaAwaAnd-shared.uninstantiated.mjs';
+
+
+const exports = (await instantiate({
+})).exports;
+
+export const {
+memory,
+_initialize
+} = exports
+
+
