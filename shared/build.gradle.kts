ccc6fdd2c9a6f135b37d256c89092e759e8f6706
rocas <rocas.spb@gmail.com>
2026-05-24 19:30:55 +0200

    update to the new KMP structure

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
-    androidTarget {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        }
+    androidLibrary {
+        namespace = "com.rocasspb.avaawaand.shared"
+        compileSdk = 36
+        minSdk = 33
     }
     
     @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
@@ -52,16 +52,4 @@ tasks.register("deliverWasm") {
     group = "delivery"
     description = "Delivers development Wasm build to web folder (alias for deliverWasmDev)"
     dependsOn("deliverWasmDev")
-}
-
-android {
-    namespace = "com.rocasspb.avaawaand.shared"
-    compileSdk = 36
-    defaultConfig {
-        minSdk = 33
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
 }
