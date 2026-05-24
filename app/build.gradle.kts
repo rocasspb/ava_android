b2d1645a4f1b1691bdba916ebd74b323277f9d9e
rocas <rocas.spb@gmail.com>
2026-05-24 19:35:11 +0200

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,21 +43,22 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
-    kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        }
-    }
-
     buildFeatures {
         buildConfig = true
         compose = true
     }
+
     testOptions {
         unitTests {
             isIncludeAndroidResources = true
             isReturnDefaultValues = true
         }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
 }
 
